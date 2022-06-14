324832c312c49f1d2c4d406b6970bca326d1a73e
Milla Kelhu <milla.kelhu@gmail.com>
2022-06-14 14:16:46 +0300

    Exercise 3.15

--- index.js
+++ index.js
@@ -72,11 +72,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -105,6 +106,18 @@ app.post('/api/persons', (request, response) => {
 const nameIsNotUnique = ( name ) => {
     return persons.some(person => person.name === name)
 }
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
