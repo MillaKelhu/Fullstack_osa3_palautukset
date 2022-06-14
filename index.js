8e01c6d9260593d1bdf7e1122ebeb1c7a91550eb
Milla Kelhu <milla.kelhu@gmail.com>
2022-06-14 14:25:49 +0300

    Exercise 3.16

--- index.js
+++ index.js
@@ -62,14 +62,16 @@ app.get('/info', (request, response) => {
         <p>${date}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
